865c748a74800f7a5518c3fcf35d6c8e512239f3
Viktor Nagy <viktor.nagy@gmail.com>
2009-12-05 22:17:37 +0100

    Added captureas template tag.

--- sugar/widgets/widget.py
+++ sugar/widgets/widget.py
@@ -2,7 +2,8 @@ from django import forms
 from django.conf import settings
 
 class GUITextAreaWidget(forms.Textarea):
+    pass
+
     class Media:
         js = ("js/jquery.wymeditor.min.js",
               "js/fancyedit.js")
-    
