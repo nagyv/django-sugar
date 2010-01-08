81f8e029f85023ced6464fd6c4cd88574572886a
Viktor Nagy <viktor.nagy@gmail.com>
2010-01-08 09:29:29 +0100

    ButtonWidget and SubmitButtonWidget added

--- sugar/widgets/widget.py
+++ sugar/widgets/widget.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.conf import settings
+from django.utils.safestring import mark_safe
+from django.forms.util import flatatt
 
 class GUITextAreaWidget(forms.Textarea):
     pass
@@ -7,3 +9,15 @@ class GUITextAreaWidget(forms.Textarea):
     class Media:
         js = ("js/jquery.wymeditor.min.js",
               "js/fancyedit.js")
+        
+class ButtonWidget(forms.widgets.Widget):
+    
+    button_type = None
+    
+    def render(self, name, value, attrs=None):
+        final_attrs = self.build_attrs(attrs, type=self.button_type, name=name)
+        return mark_safe(u'<button%s>%s</button>' % (flatatt(final_attrs), value))
+    
+class SubmitButtonWidget(ButtonWidget):
+    
+    button_type = 'submit'
